007328c7e8ee8a58b59416cf7cc1f5092c737082
Faloniang <fafania129@gmil.com>
2019-11-01 14:04:23 +0000

    Update Commit

--- TestProject/settings.py
+++ TestProject/settings.py
@@ -153,6 +153,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
